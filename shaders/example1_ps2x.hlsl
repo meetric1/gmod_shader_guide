040045f550c0fb9daa58befb68721d4370b6f1c0
Meetric <73554599+meetric1@users.noreply.github.com>
2025-05-22 00:36:52 -0400

    Update example1_ps2x.hlsl

--- shaders/example1_ps2x.hlsl
+++ shaders/example1_ps2x.hlsl
@@ -1,10 +1,10 @@
 // Simple shader that returns bright green
-// As of right now we're just trying to compile this shader, though you may feel free to take a look
+// As of right now we're just trying to compile this shader, though feel free to take a look
 struct PS_INPUT {
 	float2 p  : VPOS;
 	float2 uv : TEXCOORD0;
 };
 
 float4 main(PS_INPUT frag) : COLOR {
-	return float4(1.0, 0.0, 0.0, 1.0);
+	return float4(0.0, 1.0, 0.0, 1.0);
 };
