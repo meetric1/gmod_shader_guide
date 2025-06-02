f9c70ce5e2252f4e871378bc0e256d7b15817a60
Meetric <73554599+meetric1@users.noreply.github.com>
2025-06-02 17:11:27 -0400

    idk

--- shaders/example13_ps2x.hlsl
+++ shaders/example13_ps2x.hlsl
@@ -1,13 +1,4 @@
-//sampler3D BASETEXTURE : register(s0);
-
-// potential fix: sampler3D doesn't work on AMD cards?
-Texture3D BASETEXTURE : register(s0);
-SamplerState SAMPLER_STATE {
-    Filter = MIN_MAG_MIP_LINEAR;
-    AddressU = WRAP;
-    AddressV = WRAP;
-	AddressW = WRAP;
-};
+sampler3D BASETEXTURE : register(s0);
 
 float4 C0 : register(c0); // c0_x = UV scale
 
@@ -20,8 +11,7 @@ float4 main(PS_INPUT frag) : COLOR {
 	float uv_scale = C0.x;
 
 	float3 final_color;
-	//final_color.xyz = tex3D(BASETEXTURE, frag.pos / uv_scale).zzz;	// Only display red channel
-	final_color.xyz = BASETEXTURE.Sample(SAMPLER_STATE, frag.pos / uv_scale).zzz;	// Only display red channel
+	final_color.xyz = tex3D(BASETEXTURE, frag.pos / uv_scale).zzz;	// Only display blue channel
 
 	return float4(final_color, 1.0);
 };
